d28ecfe2b1946df7c647c195965c70fc03a336eb
Farzad Daud <daud.farzad1@gmail.com>
2020-03-02 21:55:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,3 +10,9 @@ end
 def print_first_directors_movie_titles
 end
 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+ require 'directors_database'
+ require 'pp'
+
+ def pretty_print_nds(nds)
+   pp nds
